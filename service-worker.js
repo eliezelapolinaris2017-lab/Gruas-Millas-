8973c6b59abf7afbfe42ad6fa31ac0646253931e
Facturas <eliezelapolinaris2017@gmail.com>
2026-01-29 09:42:26 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,2 +1,32 @@
-self.addEventListener("install", e => self.skipWaiting());
-self.addEventListener("fetch", e => {});
+const CACHE_NAME = "nexus-transport-v1";
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./manifest.json",
+  "./assets/logo.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((k) => (k !== CACHE_NAME ? caches.delete(k) : null)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
